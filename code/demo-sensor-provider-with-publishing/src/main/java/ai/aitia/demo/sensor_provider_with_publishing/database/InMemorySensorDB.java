de8a86d298d911d0b79a8c7ecbf7b35a86b305df
JuliCSD <juliana.c.sanchez.d@gmail.com>
2025-01-06 18:32:48 +0100

    modif bd pendant getter

--- code/demo-sensor-provider-with-publishing/src/main/java/ai/aitia/demo/sensor_provider_with_publishing/database/InMemorySensorDB.java
+++ code/demo-sensor-provider-with-publishing/src/main/java/ai/aitia/demo/sensor_provider_with_publishing/database/InMemorySensorDB.java
@@ -2,6 +2,8 @@ package ai.aitia.demo.sensor_provider_with_publishing.database;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.springframework.stereotype.Component;
 
@@ -23,6 +25,9 @@ public class InMemorySensorDB extends ConcurrentHashMap<Integer, Sensor> {
 	
 	private int idCounter = 0;
 
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+
 	public InMemorySensorDB() {
 		initializeSensors();
 	}
@@ -33,6 +38,8 @@ public class InMemorySensorDB extends ConcurrentHashMap<Integer, Sensor> {
 	//-------------------------------------------------------------------------------------------------
 
 	private void initializeSensors() {
+		lock.readLock().lock();
+
 		try (BufferedReader br = new BufferedReader(new FileReader("demo-sensor-provider-with-publishing/target/test.csv"))) {
 			String line;
 			while ((line = br.readLine()) != null) {
@@ -46,59 +53,87 @@ public class InMemorySensorDB extends ConcurrentHashMap<Integer, Sensor> {
 
 		} catch (IOException e) {
 			throw new InvalidParameterException("Error reading CSV file", e);
-		}
+		} finally {
+            lock.readLock().unlock();
+        }
 	}
 
 	public Sensor create(final String name, final String value) {
+		lock.readLock().lock();
+        try {
+			if (name == null || name.isBlank()) {
+				throw new InvalidParameterException("name is null or empty");
+			}		
+			if (value == null || value.isBlank()) {
+				throw new InvalidParameterException("value is null or empty");
+			}
+			
+			idCounter++;
+			this.put(idCounter, new Sensor(idCounter, name.toLowerCase().trim(), value.toLowerCase().trim()));
 
-		if (name == null || name.isBlank()) {
-			throw new InvalidParameterException("name is null or empty");
-		}		
-		if (value == null || value.isBlank()) {
-			throw new InvalidParameterException("value is null or empty");
-		}
-		
-		idCounter++;
-		this.put(idCounter, new Sensor(idCounter, name.toLowerCase().trim(), value.toLowerCase().trim()));
+		} finally {
+            lock.readLock().unlock();
+        }
 		return this.get(idCounter);
 	}
 	
 	//-------------------------------------------------------------------------------------------------
 	public List<Sensor> getAll() {
-		return List.copyOf(this.values());
+		lock.readLock().lock();
+        try {
+			return List.copyOf(this.values());
+		} finally {
+            lock.readLock().unlock();
+        }
 	}
 	
 	//-------------------------------------------------------------------------------------------------
 	public Sensor getById(final int id) {
-		if (this.containsKey(id)) {
-			return this.get(id);
-		} else {
-			throw new InvalidParameterException("id '" + id + "' not exists");
-		}
+		lock.writeLock().lock();
+		try{
+			if (this.containsKey(id)) {
+				return this.get(id);
+			} else {
+				throw new InvalidParameterException("id '" + id + "' not exists");
+			}
+		} finally {
+            lock.writeLock().unlock();
+        }
 	}
 	
 	//-------------------------------------------------------------------------------------------------
 	public Sensor updateById(final int id, final String name, final String value) {
-		if (this.containsKey(id)) {
-			final Sensor sensor = this.get(id);
-			if (name!= null && !name.isBlank()) {
-				sensor.setName(name);
+		lock.writeLock().lock();
+        try {
+			if (this.containsKey(id)) {
+				final Sensor sensor = this.get(id);
+				if (name!= null && !name.isBlank()) {
+					sensor.setName(name);
+				}
+				if (value != null && !value.isBlank()) {
+					sensor.setValue(value);
+				}
+				this.put(id, sensor);
+				return sensor;
+			} else {
+				throw new InvalidParameterException("id '" + id + "' not exists");
 			}
-			if (value != null && !value.isBlank()) {
-				sensor.setValue(value);
-			}
-			this.put(id, sensor);
-			return sensor;
-		} else {
-			throw new InvalidParameterException("id '" + id + "' not exists");
-		}
+		} finally {
+            lock.writeLock().unlock();
+        }
 	}
 	
 	//-------------------------------------------------------------------------------------------------
 	public void removeById(final int id) {
-		if (this.containsKey(id)) {
-			this.remove(id);
-		}
+
+		lock.writeLock().lock();
+        try {
+			if (this.containsKey(id)) {
+				this.remove(id);
+			}
+		} finally {
+            lock.writeLock().unlock();
+        }
 	}
 
 }
